067cf061deeea15f866e9695a1d76b2710b694f5
pastaChaeng <123816211+pastaChaeng@users.noreply.github.com>
2023-06-23 13:55:46 +0800

    js | changes

--- script.js
+++ script.js
@@ -4,14 +4,47 @@ function App(){
     const [randomQuote, setRandomQuotes] = React.useState([]);
 
     React.useEffect(() =>{
-    async function fectData(){
+    async function fetchData(){
         const response = await fetch("https://type.fit/api/quotes/")
         const data = await response.json();
-    }
+
+        setQuotes(data);
+        let randIndex = Math.floor(Math.random() * data.length);
+        setRandomQuote(data[randIndex])
+    }   
+    fetchData();
     }, [])
 
-    return(
-    <div>Hello World</div>
+    const getNewQuote = () => {
+        let randIndex = Math.floor(Math.random() * quotes.length);
+        setRandomQuote(data[randIndex])
+    }
+
+    return (
+    <div className="container pt-5">
+        <div className="jumbotron">
+            <div className="card">
+            <div className="card-header">Taylor's Quote</div>
+            <div className="card-body">
+                {randomQuote ? (
+                    <>
+                    <h5 className="card-title">- {randomQuote.author || "NO author"}</h5>
+                    <p className="card-text">&qout;{randomQuote.text}&qout;</p>
+                  </>
+                ):(
+                    <h2>Loading</h2>
+                )}
+                <div className="row">
+                    <button onCLick={getNewQuote} className="btn btn-primary ml-3">New Quote</button>
+                    <a href=""></a>
+                    <a href=""></a>
+
+            </div>
+        </div>
+        </div>
+        </div>
+       hello
+    </div>
     );
 }
 
