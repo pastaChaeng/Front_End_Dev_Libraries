f78f590562acd16bb9bff75aae342b59d42bad9d
pastaChaeng <123816211+pastaChaeng@users.noreply.github.com>
2023-06-23 14:57:47 +0800

    js | changes1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function App(){
 
     const [quotes, setQuotes] = React.useState([]);
-    const [randomQuote, setRandomQuotes] = React.useState([]);
+    const [randomQuote, setRandomQuote] = React.useState("");
 
     React.useEffect(() =>{
     async function fetchData(){
@@ -17,33 +17,34 @@ function App(){
 
     const getNewQuote = () => {
         let randIndex = Math.floor(Math.random() * quotes.length);
-        setRandomQuote(data[randIndex])
+        setRandomQuote(quotes[randIndex])
     }
 
     return (
     <div className="container pt-5">
         <div className="jumbotron">
             <div className="card">
-            <div className="card-header">Taylor's Quote</div>
+            <div className="card-header">Inspirational Quotes</div>
             <div className="card-body">
                 {randomQuote ? (
                     <>
                     <h5 className="card-title">- {randomQuote.author || "NO author"}</h5>
-                    <p className="card-text">&qout;{randomQuote.text}&qout;</p>
+                    <p className="card-text">&quot;{randomQuote.text}&quot;</p>
                   </>
-                ):(
+                ) : (
                     <h2>Loading</h2>
                 )}
                 <div className="row">
-                    <button onCLick={getNewQuote} className="btn btn-primary ml-3">New Quote</button>
-                    <a href=""></a>
-                    <a href=""></a>
+                    <button className="btn btn-primary ml-3">New Quote</button>
+                    <a href="" className="btn btn-warning">
+                        <i className="fa fa-twitter"> </i>
+                    </a>
+                    <a href="" className="btn btn-danger"></a>
 
             </div>
         </div>
         </div>
         </div>
-       hello
     </div>
     );
 }
