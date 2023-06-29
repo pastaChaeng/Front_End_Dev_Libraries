516cd20c2515dc06c78d66265e6902588168bec1
Arlene Joy Nacion <123816211+pastaChaeng@users.noreply.github.com>
2023-06-29 09:17:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function App(){
     const [randomQuote, setRandomQuote] = React.useState("");
 
     React.useEffect(() =>{
-    async function fetchData(){
-        const response = await fetch("https://type.fit/api/quotes/")
-        const data = await response.json();
+        async function fetchData(){
+            const response = await fetch("https://type.fit/api/quotes/")
+            const data = await response.json();
 
-        setQuotes(data);
-        let randIndex = Math.floor(Math.random() * data.length);
-        setRandomQuote(data[randIndex])
-    }   
-    fetchData();
+            setQuotes(data);
+            let randIndex = Math.floor(Math.random() * data.length);
+            setRandomQuote(data[randIndex])
+        }   
+        fetchData();
     }, [])
 
     const getNewQuote = () => {
@@ -21,31 +21,30 @@ function App(){
     }
 
     return (
-    <div className="container pt-5">
-        <div className="jumbotron">
-            <div className="card">
-            <div className="card-header">Inspirational Quotes</div>
-            <div className="card-body">
-                {randomQuote ? (
-                    <>
-                    <h5 className="card-title">- {randomQuote.author || "NO author"}</h5>
-                    <p className="card-text">&quot;{randomQuote.text}&quot;</p>
-                  </>
-                ) : (
-                    <h2>Loading</h2>
-                )}
-                <div className="row">
-                    <button className="btn btn-primary ml-3">New Quote</button>
-                    <a href="" className="btn btn-warning">
-                        <i className="fa fa-twitter"> </i>
-                    </a>
-                    <a href="" className="btn btn-danger"></a>
-
+        <div className="container pt-5">
+            <div className="jumbotron">
+                <div className="card">
+                    <div className="card-header">Inspirational Quotes</div>
+                    <div className="card-body">
+                        {randomQuote ? (
+                            <>
+                                <h5 className="card-title">- {randomQuote.author || "NO author"}</h5>
+                                <p className="card-text">&quot;{randomQuote.text}&quot;</p>
+                            </>
+                        ) : (
+                            <h2>Loading</h2>
+                        )}
+                        <div className="row">
+                            <button className="btn btn-primary ml-3" onClick={getNewQuote}>New Quote</button>
+                            <a href={`https://twitter.com/intent/tweet?text=${randomQuote.text}`} className="btn btn-warning">
+                                <i className="fa fa-twitter"> </i>
+                            </a>
+                            <a href="" className="btn btn-danger"></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
-        </div>
-    </div>
     );
 }
 
